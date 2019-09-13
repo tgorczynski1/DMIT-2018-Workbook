6b6344f4834fdd05bab8aca0fd223efce8359a37
Thomas C Gorczynski <tgorczynski1@nait.ca>
2019-09-13 13:02:33 -0600

    fack

--- src/Code-First from Database/WestWindSystem/DAL/WestWindContext.cs
+++ src/Code-First from Database/WestWindSystem/DAL/WestWindContext.cs
@@ -1,9 +1,10 @@
-namespace WestWindSystem.Entities
+namespace WestWindSystem.DAL
 {
     using System;
     using System.Data.Entity;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
+    using WestWindSystem.Entities;
 
     public partial class WestWindContext : DbContext
     {
