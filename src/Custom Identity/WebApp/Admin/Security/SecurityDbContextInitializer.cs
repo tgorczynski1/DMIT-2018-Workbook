b2dbe669caffd227ed3129518d76df47d8d04bf7
tgorczynski1 <46577121+tgorczynski1@users.noreply.github.com>
2019-11-01 13:11:46 -0600

    added class materials

---
+++ src/Custom Identity/WebApp/Admin/Security/SecurityDbContextInitializer.cs
@@ -0,0 +1,81 @@
+﻿using DemoSystem.BLL;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApp.Models;
+
+// You can learn about Database Initialization Strategies in EF6 via
+// http://www.entityframeworktutorial.net/code-first/database-initialization-strategy-in-code-first.aspx
+
+namespace WebApp.Admin.Security
+{
+    /// <summary>
+    /// Provide functionality for setting up the database for the ApplicationDbContext.
+    /// The specific functionality is to create the database if it does not exist.
+    /// </summary>
+    public class SecurityDbContextInitializer
+        : CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            // To "seed" a database is to provide it with some initial data
+            // when the database is created.
+
+            #region Seed the security roles
+            // Make the Identity's BLL instance to help us manage roles
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            // The RoleManager<T> and RoleStore<T> are BLL classes that give flexibility
+            // to the design/structure of how we're using Asp.NET Identity.
+            // The IdentityRole is an Entity class that represents a security role.
+
+            // TODO: Move these hard-coded security roles to Web.config
+            roleManager.Create(new IdentityRole { Name = "Administrators" });
+            roleManager.Create(new IdentityRole { Name = "Registered Users" });
+            #endregion
+
+            #region Seed the users
+            var adminUser = new ApplicationUser
+            {
+                UserName = "WebAdmin",
+                Email = "Elections2020@Hackers.ru",
+                EmailConfirmed = true
+            };
+            #endregion
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+
+            var result = userManager.Create(adminUser, "Pa$$w0rd");
+            if (result.Succeeded)
+            {
+                var adminId = userManager.FindByName("WebAdmin").Id;
+
+                userManager.AddToRole(adminId, "Administrators");
+            }
+
+            var demoManager = new DemoController();
+            var people = demoManager.ListPeople();
+            foreach(var person in people)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = $"{person.FirstName}.{person.LastName}",
+                    Email = $"{person.FirstName}.{person.LastName}@DemoIsland.com",
+                    EmailConfirmed = true,
+                    PersonId = person.PersonID
+                };
+                result = userManager.Create(user, "Pa$$w0rd");
+                if (result.Succeeded)
+                {
+                    var userId = userManager.FindByName(user.UserName).Id;
+                    userManager.AddToRole(userId, "Registered Users");
+                }
+            }
+
+            // Note: Keep this call to the base class so it can do its seeding work
+            base.Seed(context);
+        }
+    }
+}
