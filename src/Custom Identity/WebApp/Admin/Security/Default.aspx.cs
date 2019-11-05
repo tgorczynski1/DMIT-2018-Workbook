4605db3e8c5ff4062348064ada85dc9ccefe6478
tgorczynski1 <46577121+tgorczynski1@users.noreply.github.com>
2019-11-05 16:38:27 -0700

    continue

---
+++ src/Custom Identity/WebApp/Admin/Security/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.Admin.Security
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //redirects user from security page if they are not an admin
+            if (!Request.IsAuthenticated || !User.IsInRole(Settings.AdminRole))
+                Response.Redirect("~/", true);
+        }
+    }
+}
--- src/Custom Identity/WebApp/Site.Master.cs
+++ src/Custom Identity/WebApp/Site.Master.cs
@@ -7,6 +7,7 @@ using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.Identity;
+using WebApp.Admin.Security;
 
 namespace WebApp
 {
@@ -72,6 +73,11 @@ namespace WebApp
 
         }
 
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            SecurityAdmin.Visible = Request.IsAuthenticated && Page.User.IsInRole(Settings.AdminRole);
+
+        }
         protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
         {
             Context.GetOwinContext().Authentication.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
