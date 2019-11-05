96ff31d504b970f2e97703eeeeeded318248af55
tgorczynski1 <46577121+tgorczynski1@users.noreply.github.com>
2019-11-05 15:37:22 -0700

    Using Teacher Example

---
+++ src/Custom Identity/WebApp/Admin/Security/Settings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Admin.Security
+{
+    public class Settings
+    {
+        #region Security Roles
+        public static string AdminRole => ConfigurationManager.AppSettings["adminRole"];
+        /* The above is the same as typing:
+         * public static string AdminRole
+         * {
+         *      get { return ConfigurationManager.AppSettings["adminRole"];
+         * }
+         */
+        public static string UserRole => ConfigurationManager.AppSettings["userRole"];
+        #endregion
+
+        #region Startup Roles
+        #endregion
+    }
+}
